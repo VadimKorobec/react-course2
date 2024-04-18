4d5ef6739c657a277ad3d1aa36481638fef238ba
VadimKorobec <_s_e_t__@ukr.net>
2024-04-18 14:31:57 +0300

    add connect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+import { createStore } from "redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
